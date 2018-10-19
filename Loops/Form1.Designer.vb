0b223b3e2caf8ffa486a1db6d044977887bc9287
Derrick Namisi Siiya <lordsiiya@gmail.com>
2018-10-19 15:26:12 +0300

    First commit of the original project

---
+++ Loops/Form1.Designer.vb
@@ -0,0 +1,312 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnLoop = New System.Windows.Forms.Button()
+        Me.listLoop = New System.Windows.Forms.ListBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnBeep = New System.Windows.Forms.Button()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.btnStepLoop = New System.Windows.Forms.Button()
+        Me.listLoop2 = New System.Windows.Forms.ListBox()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.lblTitle2 = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.btnWCloop = New System.Windows.Forms.Button()
+        Me.listWorldCup = New System.Windows.Forms.ListBox()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.btnPictureLoop = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.TabPage4.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 19)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(489, 300)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Panel2)
+        Me.TabPage1.Controls.Add(Me.Panel1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(481, 274)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Beep Loop"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.LightGray
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.btnLoop)
+        Me.Panel2.Controls.Add(Me.listLoop)
+        Me.Panel2.Location = New System.Drawing.Point(244, 15)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(200, 234)
+        Me.Panel2.TabIndex = 2
+        '
+        'btnLoop
+        '
+        Me.btnLoop.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLoop.Location = New System.Drawing.Point(3, 13)
+        Me.btnLoop.Name = "btnLoop"
+        Me.btnLoop.Size = New System.Drawing.Size(181, 53)
+        Me.btnLoop.TabIndex = 1
+        Me.btnLoop.Text = "FOR LOOP"
+        Me.btnLoop.UseVisualStyleBackColor = True
+        '
+        'listLoop
+        '
+        Me.listLoop.FormattingEnabled = True
+        Me.listLoop.Location = New System.Drawing.Point(3, 72)
+        Me.listLoop.Name = "listLoop"
+        Me.listLoop.Size = New System.Drawing.Size(181, 147)
+        Me.listLoop.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.btnBeep)
+        Me.Panel1.Location = New System.Drawing.Point(6, 15)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(218, 234)
+        Me.Panel1.TabIndex = 1
+        '
+        'btnBeep
+        '
+        Me.btnBeep.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBeep.Location = New System.Drawing.Point(33, 46)
+        Me.btnBeep.Name = "btnBeep"
+        Me.btnBeep.Size = New System.Drawing.Size(154, 64)
+        Me.btnBeep.TabIndex = 0
+        Me.btnBeep.Text = "BEEP SOUND"
+        Me.btnBeep.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Panel3)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(481, 274)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Step LOOP"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.LightGray
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.btnStepLoop)
+        Me.Panel3.Controls.Add(Me.listLoop2)
+        Me.Panel3.Location = New System.Drawing.Point(140, 10)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(200, 234)
+        Me.Panel3.TabIndex = 3
+        '
+        'btnStepLoop
+        '
+        Me.btnStepLoop.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStepLoop.Location = New System.Drawing.Point(3, 13)
+        Me.btnStepLoop.Name = "btnStepLoop"
+        Me.btnStepLoop.Size = New System.Drawing.Size(181, 53)
+        Me.btnStepLoop.TabIndex = 1
+        Me.btnStepLoop.Text = "STEP LOOP"
+        Me.btnStepLoop.UseVisualStyleBackColor = True
+        '
+        'listLoop2
+        '
+        Me.listLoop2.FormattingEnabled = True
+        Me.listLoop2.Location = New System.Drawing.Point(3, 72)
+        Me.listLoop2.Name = "listLoop2"
+        Me.listLoop2.Size = New System.Drawing.Size(181, 147)
+        Me.listLoop2.TabIndex = 0
+        '
+        'TabPage3
+        '
+        Me.TabPage3.BackColor = System.Drawing.Color.LightGray
+        Me.TabPage3.Controls.Add(Me.Panel4)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(481, 274)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "World Cup Loop"
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.LightGray
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel4.Controls.Add(Me.lblTitle2)
+        Me.Panel4.Controls.Add(Me.lblTitle)
+        Me.Panel4.Controls.Add(Me.btnWCloop)
+        Me.Panel4.Controls.Add(Me.listWorldCup)
+        Me.Panel4.Location = New System.Drawing.Point(140, 3)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(249, 241)
+        Me.Panel4.TabIndex = 4
+        '
+        'lblTitle2
+        '
+        Me.lblTitle2.AutoSize = True
+        Me.lblTitle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle2.ForeColor = System.Drawing.Color.Blue
+        Me.lblTitle2.Location = New System.Drawing.Point(38, 81)
+        Me.lblTitle2.Name = "lblTitle2"
+        Me.lblTitle2.Size = New System.Drawing.Size(155, 16)
+        Me.lblTitle2.TabIndex = 3
+        Me.lblTitle2.Text = "in the following Years"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.Blue
+        Me.lblTitle.Location = New System.Drawing.Point(3, 67)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(251, 16)
+        Me.lblTitle.TabIndex = 2
+        Me.lblTitle.Text = "Word Cup Tournaments take place "
+        '
+        'btnWCloop
+        '
+        Me.btnWCloop.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnWCloop.Location = New System.Drawing.Point(34, 1)
+        Me.btnWCloop.Name = "btnWCloop"
+        Me.btnWCloop.Size = New System.Drawing.Size(181, 63)
+        Me.btnWCloop.TabIndex = 1
+        Me.btnWCloop.Text = "WORLD CUP YEARS"
+        Me.btnWCloop.UseVisualStyleBackColor = True
+        '
+        'listWorldCup
+        '
+        Me.listWorldCup.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.listWorldCup.ForeColor = System.Drawing.Color.Blue
+        Me.listWorldCup.FormattingEnabled = True
+        Me.listWorldCup.ItemHeight = 25
+        Me.listWorldCup.Location = New System.Drawing.Point(41, 100)
+        Me.listWorldCup.Name = "listWorldCup"
+        Me.listWorldCup.Size = New System.Drawing.Size(181, 129)
+        Me.listWorldCup.TabIndex = 0
+        '
+        'TabPage4
+        '
+        Me.TabPage4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TabPage4.Controls.Add(Me.btnPictureLoop)
+        Me.TabPage4.Controls.Add(Me.PictureBox1)
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Size = New System.Drawing.Size(481, 274)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "Picture Loop"
+        '
+        'btnPictureLoop
+        '
+        Me.btnPictureLoop.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPictureLoop.Location = New System.Drawing.Point(126, 229)
+        Me.btnPictureLoop.Name = "btnPictureLoop"
+        Me.btnPictureLoop.Size = New System.Drawing.Size(200, 42)
+        Me.btnPictureLoop.TabIndex = 1
+        Me.btnPictureLoop.Text = "NEXT PICTURE"
+        Me.btnPictureLoop.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.PictureBox1.BackgroundImage = Global.Loops.My.Resources.Resources.cloud
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(24, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(437, 222)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(504, 347)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "USING LOOPS IN VB"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.TabPage4.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnBeep As System.Windows.Forms.Button
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents btnLoop As System.Windows.Forms.Button
+    Friend WithEvents listLoop As System.Windows.Forms.ListBox
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage4 As System.Windows.Forms.TabPage
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents btnStepLoop As System.Windows.Forms.Button
+    Friend WithEvents listLoop2 As System.Windows.Forms.ListBox
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents lblTitle2 As System.Windows.Forms.Label
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents btnWCloop As System.Windows.Forms.Button
+    Friend WithEvents listWorldCup As System.Windows.Forms.ListBox
+    Friend WithEvents btnPictureLoop As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Loops/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+
+    Private Sub btnBeep_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBeep.Click
+
+        '. Loop that creates a beep sound when the Beep button is clicked
+
+        Dim i As Integer
+
+        For i = 1 To 5
+            Beep()
+        Next i
+
+    End Sub
+
+    Private Sub btnLoop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLoop.Click
+        '. For loop which displays numbers from 1 to 200 in a listbox
+        '    .
+        Dim i As Integer
+
+        For i = 1 To 200
+            listLoop.Items.Add(i)
+
+        Next i
+    End Sub
+
+    Private Sub btnStepLoop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStepLoop.Click
+        '. The loop below loops through numbers 1 to 20 using a Step Index of 2, the loop results are added to the listLoop2 listbox
+        Dim i As Integer
+
+        For i = 1 To 200 Step 2
+            listLoop2.Items.Add(i)
+
+        Next i
+
+    End Sub
+
+    Private Sub btnWCloop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnWCloop.Click
+        '. The loop (leapYear) uses Step Index of 4 to loop
+        '.through the numbers (years) 1994 to 2300, where the later is the End point and the former the Start point, the loop results are added to a listbox
+
+        Dim loopYear As Integer
+
+        For loopYear = 1994 To 2300 Step 4
+            listWorldCup.Items.Add(loopYear)
+
+        Next loopYear
+
+    End Sub
+
+    Private Sub btnPictureLoop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPictureLoop.Click
+
+
+        '. The codes below creates a For loop (i) which is able to loop 
+        '    . through images 1- 3
+        '    .with the aid of a Message box
+        '    .
+        '    .
+        Dim i As Integer
+
+        For i = 1 To 3
+            PictureBox1.Image = System.Drawing.Image.FromFile("C:\loopPictures\image" & i & ".jpg")
+
+            MessageBox.Show("Click here for the next image")
+        Next i
+
+    End Sub
+
+End Class
